7a92f9f333104d808945ce4ac8348749e6e56930
Suco12 <98502796+Suco12@users.noreply.github.com>
2026-03-17 09:21:29 +0000

    Remove nested repository and add to gitignore

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ st.header('Análise de veículos nos EUA')
 
 car_data = pd.read_csv('vehicles_us.csv')
 hist_button = st.button('Gerar gráfico de distribuição de marcas de veículos')
-
 if hist_button:
     st.write('Criando histograma para a coluna odometer')
     fig = px.histogram(car_data, x='odometer', nbins=50, title='Distribuição de odômetro dos veículos')
