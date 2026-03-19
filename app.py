79e22fe14cc3478e987aa5622b95054b8099f65c
Suco12 <98502796+Suco12@users.noreply.github.com>
2026-03-19 21:41:50 +0000

    Descrição das alterações

--- app.py
+++ app.py
@@ -2,11 +2,22 @@ import streamlit as st
 import pandas as pd 
 import plotly.express as px  # Correção aqui!
 
+# Carregando os dados:
+car_data = pd.read_csv('vehicles_us.csv')
+
+# Cabeçalho 
 st.header('Análise de veículos nos EUA')
 
-car_data = pd.read_csv('vehicles_us.csv')
-hist_button = st.button('Gerar gráfico de distribuição de marcas de veículos')
+# Botão para histograma
+hist_button = st.button('Gerar gráfico de distribuição de odômetro')
 if hist_button:
     st.write('Criando histograma para a coluna odometer')
     fig = px.histogram(car_data, x='odometer', nbins=50, title='Distribuição de odômetro dos veículos')
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, use_container_width=True)
+
+# Botão para gráfico de dispersão
+scatter_button = st.button('Gerar gráfico de dispersão')
+if scatter_button:
+    st.write('Criando gráfico de dispersão entre odômetro e preço')
+    fig = px.scatter(car_data, x = 'odometer', y = 'price', title = 'Dispersão entre odômetro e preço dos veículos')
+    st.plotly_chart(fig, use_container_width=True)
